b4545a98f7a3d4fa4a04fbcd74bbf6275947e7c0
khaldor <khaldor@khaldors-iMac.local>
2018-03-16 03:32:11 -0700

    Added pods and google sing in

--- EpitechEisenhower/EpitechEisenhower/LoginViewController.swift
+++ EpitechEisenhower/EpitechEisenhower/LoginViewController.swift
@@ -7,17 +7,32 @@
 //
 
 import UIKit
+import GoogleSignIn
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, GIDSignInUIDelegate {
 
     @IBOutlet weak var connectButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Login"
         connectButton.layer.cornerRadius = 5
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().signIn()
+        let googleSignInButton = GIDSignInButton()
+        googleSignInButton.center = view.center
+        view.addSubview(googleSignInButton)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if (error != nil) {
+            print("Google sign in error")
+        }
+        else {
+            self.performSegue(withIdentifier: "showHome", sender: nil)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
