add798bc398c18306e83be09cfccdf612a861f2b
Mc Fly <mcfly@Mcs-iMac.local>
2018-03-19 13:24:00 -0700

    all pages

---
+++ EpitechEisenhower/EpitechEisenhower/YourProfileViewController.swift
@@ -0,0 +1,14 @@
+//
+//  YourProfileViewController.swift
+//  EpitechEisenhower
+//
+//  Created by Mc Fly on 19/03/2018.
+//  Copyright © 2018 Epitech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class YourProfileViewController: UIViewController {
+    
+}
