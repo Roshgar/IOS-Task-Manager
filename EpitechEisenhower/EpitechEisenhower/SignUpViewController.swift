9a8c7728132ee12ba1fe3720b45b5f3ab1b37453
khaldor <khaldor@khaldors-iMac.local>
2018-04-02 10:04:13 -0700

    Added signUp controller

---
+++ EpitechEisenhower/EpitechEisenhower/SignUpViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpController.swift
+//  EpitechEisenhower
+//
+//  Created by khaldor on 02/04/2018.
+//  Copyright © 2018 Epitech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    @IBOutlet weak var login: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+}
