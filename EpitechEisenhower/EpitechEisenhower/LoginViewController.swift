88512f0c801e422d4b73414e035d2e6fc355084d
khaldor <khaldor@khaldors-iMac.local>
2018-04-02 12:42:48 -0700

    Added sign in

--- EpitechEisenhower/EpitechEisenhower/LoginViewController.swift
+++ EpitechEisenhower/EpitechEisenhower/LoginViewController.swift
@@ -16,6 +16,8 @@ import FirebaseDatabase
 
 class LoginViewController: UIViewController, GIDSignInUIDelegate {
     
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var googleConnectButton: GIDSignInButton!;
     @IBOutlet weak var connectButton: UIButton!
     var refDB : DatabaseReference!;
@@ -101,10 +103,43 @@ class LoginViewController: UIViewController, GIDSignInUIDelegate {
         GIDSignIn.sharedInstance().signIn()
     }
     
-    // segue showHome is executed twice, TOFIX
-    @IBAction func connect(_ sender: Any) {
-        performSegue(withIdentifier: "showHome", sender: nil)
+    @IBAction func logInFirebase(_ sender: Any) {
+        if self.emailTextField.text == "" || self.passwordTextField.text == "" {
+            
+            //Alert to tell the user that there was an error because they didn't fill anything in the textfields because they didn't fill anything in
+            
+            let alertController = UIAlertController(title: "Error", message: "Please enter an email and password.", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+            
+        } else {
+            
+            Auth.auth().signIn(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!) { (user, error) in
+                
+                if error == nil {
+                    
+                    //Print into the console if successfully logged in
+                    print("You have successfully logged in")
+                    
+                    //Go to the HomeViewController if the login is sucessful
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "HomeViewController")
+                    self.present(vc!, animated: true, completion: nil)
+                    
+                } else {
+                    
+                    //Tells the user that there is an error and then gets firebase to tell them the error
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
     }
-
 }
 
