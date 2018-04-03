bec3fc842bfd191c94c657862f278a98e93f7af0
Mc Fly <mcfly@Mcs-iMac.local>
2018-04-03 10:13:49 -0700

    people search bar

---
+++ EpitechEisenhower/EpitechEisenhower/SearchTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  SearchTableViewCell.swift
+//  
+//
+//  Created by Mc Fly on 03/04/2018.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    
+    
+    func viewDidLoad() {
+        //super.viewDidLoad()
+    
+        
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
