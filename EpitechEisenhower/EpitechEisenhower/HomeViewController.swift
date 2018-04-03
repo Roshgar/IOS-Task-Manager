f02a9dcac725e07cc0c7bf1983607cc8b690076c
khaldor <khaldor@khaldors-iMac.local>
2018-04-02 17:57:45 -0700

    Task adding and retrieval completed

--- EpitechEisenhower/EpitechEisenhower/HomeViewController.swift
+++ EpitechEisenhower/EpitechEisenhower/HomeViewController.swift
@@ -8,7 +8,56 @@
 
 import Foundation
 import UIKit
+import FirebaseDatabase
+import Firebase
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    let reuseIdentifier = "cell"
+    
+    
+    
+    override func viewDidLoad() {
+        let currentUser = Auth.auth().currentUser
+        let userID = Auth.auth().currentUser?.uid
+        let refDB = Database.database().reference()
+        let items = ["testItem"]
+        
+    (refDB.child("users").child(userID!).child("tasks")).observeSingleEvent(of: .value, with: { (snapshot) in
+            // Get user value
+            let value = snapshot.value as? NSDictionary
+            print("task object")
+            print(value)
+            //let username = value?["username"] as? String ?? ""
+           // let user = User(username: username)
+            
+            // ...
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1//self.items.count
+    }
+    
+    // make a cell for each cell index path
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! TaskViewCellCollectionViewCell
+        
+        // Use the outlet in our custom class to get a reference to the UILabel in the cell
+        cell.taskLabel.text = "This is but a test"
+        cell.backgroundColor = UIColor.cyan // make cell more visible in our example project
+        
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate protocol
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
+    }
 }
 
--- EpitechEisenhower/EpitechEisenhower/TaskDetailsViewController.swift
+++ EpitechEisenhower/EpitechEisenhower/TaskDetailsViewController.swift
@@ -8,10 +8,14 @@
 
 import Foundation
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class TaskDetailsViewController: UIViewController {
     
+    @IBOutlet weak var taskDesc: UITextView!
     @IBOutlet weak var taskLabel: UITextView!
+    @IBOutlet weak var dateTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +25,38 @@ class TaskDetailsViewController: UIViewController {
     
     @IBAction func AddPeopleButton(_ sender: Any) {
         
+    }
+    
+    
+    @IBAction func saveTask(_ sender: Any) {
+        let desc = taskDesc.text
+        let date = dateTextField.text
+        let label = taskLabel.text
+        let currentUser = Auth.auth().currentUser
+        let userID = Auth.auth().currentUser?.uid
+        let refDB = Database.database().reference()
+        
+        let key = refDB.child("users").child(userID!).child("posts").childByAutoId().key
+        let task = ["label" : label, "date": date, "desc": desc]
+        
+        let childUpdates = ["users/\(userID!)/tasks/\(key)" : task]
+        refDB.updateChildValues(childUpdates)
+    }
+    
+    // Because apparently DatePickers weren't thought of when designing the template.
+    @IBAction func textFieldEditing(_ sender: UITextField) {
+        let datePickerView: UIDatePicker = UIDatePicker()
+        datePickerView.datePickerMode = UIDatePickerMode.date
+        sender.inputView = datePickerView
+        datePickerView.addTarget(self, action: #selector(TaskDetailsViewController.datePickerValueChanged), for: UIControlEvents.valueChanged)
+    }
+    
+    // See above
+    @objc func datePickerValueChanged(sender:UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = DateFormatter.Style.medium
+        dateFormatter.timeStyle = DateFormatter.Style.none
+        dateTextField.text = dateFormatter.string(from: sender.date)
     }
     
     
