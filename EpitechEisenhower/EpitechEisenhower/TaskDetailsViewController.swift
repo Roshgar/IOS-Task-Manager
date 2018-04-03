38243038f06533d27cd0de4c4ce993d178bac653
Mc Fly <mcfly@Mcs-iMac.local>
2018-04-03 06:04:05 -0700

    upload image button added, search people close button added

--- EpitechEisenhower/EpitechEisenhower/TaskDetailsViewController.swift
+++ EpitechEisenhower/EpitechEisenhower/TaskDetailsViewController.swift
@@ -20,27 +20,39 @@ class TaskDetailsViewController: UIViewController {
     @IBOutlet weak var importantButton: UIButton!
     @IBOutlet weak var urgentButton: UIButton!
     
+    @IBOutlet weak var peopleSearch: UIView!
+    
+    @IBAction func openPeopleSearchButton(_ sender: Any) {
+        peopleSearch.isHidden = false;
+    }
+    
+    @IBAction func CloseSearch(_ sender: Any) {
+        peopleSearch.isHidden = true;
+    }
+    
     var importantState : Bool! = false
-    @IBAction func importantButtonClicked(_ sender: Any) {
+    @IBAction func importantButtonClicked(_ sender: UIButton) {
+        
         if (importantState) {
             //Second Tap
-            importantButton.alpha = 0.5;
+            sender.alpha = 0.5;
             importantState = false
         } else {
             //First Tap
-            importantButton.alpha = 1;
+            sender.alpha = 1;
             importantState = true
         }
     }
     var urgentState : Bool! = false
-    @IBAction func urgentButtonClicked(_ sender: Any) {
+    @IBAction func urgentButtonClicked(_ sender: UIButton) {
+        
         if (urgentState) {
             //Second Tap
-            urgentButton.alpha =  0.5;
+            sender.alpha =  0.5;
             urgentState = false
         } else {
             //First Tap
-            urgentButton.alpha = 1;
+            sender.alpha = 1;
             urgentState = true
         }
     }
@@ -63,12 +75,16 @@ class TaskDetailsViewController: UIViewController {
         let desc = taskDesc.text
         let date = dateTextField.text
         let label = taskLabel.text
+        
+        let important = String(importantState);
+        let urgent = String(urgentState);
+        
         let currentUser = Auth.auth().currentUser
         let userID = currentUser?.uid
         let refDB = Database.database().reference()
         
         let key = refDB.child("users").child(userID!).child("posts").childByAutoId().key
-        let task = ["label" : label, "date": date, "desc": desc]
+        let task = ["label" : label!, "date": date, "desc": desc, "important" : important, "urgent" : urgent]
         
         let childUpdates = ["users/\(userID!)/tasks/\(key)" : task]
         refDB.updateChildValues(childUpdates)
